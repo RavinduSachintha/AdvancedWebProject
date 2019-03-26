3d2fe65e2c4cd942cf30b43e0ea93ee88786ccc0
RavinduSachintha <ravindusachintha53@gmail.com>
2019-03-26 14:23:35 +0530

    implement the connection file

--- back-end/app.js
+++ back-end/app.js
@@ -4,25 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-// just for testing ---------------------
-mongoose.connect('mongodb+srv://sachintha:sachintha22@cluster0-uwov5.mongodb.net/test?retryWrites=true')
-    .then(() => {
-        var db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.once('open', function() {
-          var kittySchema = new mongoose.Schema({
-            name: String
-          });
-          var Kitten = mongoose.model('Kitten', kittySchema);
-          var silence = new Kitten({name: 'Sillence'});
-          console.log(silence.name);
-        })
-    })
-    .catch((err) => console.error(err));
-// ----------------------------
 
 var app = express();
 
