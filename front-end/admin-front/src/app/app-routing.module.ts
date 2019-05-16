f05ed4d11d6d0493bba567a2457b2ac20df0795f
RavinduSachintha <ravindusachintha53@gmail.com>
2019-05-16 14:53:36 +0530

    assets updated

--- front-end/admin-front/src/app/app-routing.module.ts
+++ front-end/admin-front/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./pages/login/login.component";
-import { AdminDashboardComponent } from "./pages/admin-dashboard/admin-dashboard.component";
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
   { path: "login", component: LoginComponent },
-  { path: "admin-dashboard", component: AdminDashboardComponent }
+  { path: "dashboard", component: DashboardComponent }
 ];
 
 @NgModule({
--- front-end/admin-front/src/app/app.module.ts
+++ front-end/admin-front/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./pages/login/login.component";
-import { AdminDashboardComponent } from "./pages/admin-dashboard/admin-dashboard.component";
 
 import { HttpBackendRequestService } from "./services/http-backend-request.service";
 import { AuthenticationService } from "./services/authentication.service";
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, AdminDashboardComponent],
+  declarations: [AppComponent, LoginComponent, DashboardComponent, NavBarComponent, SideBarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [HttpBackendRequestService, AuthenticationService],
   bootstrap: [AppComponent]
