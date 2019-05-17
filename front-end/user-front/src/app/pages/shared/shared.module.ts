864c1c9da7d3788c0a09c14c77fc097d371b1745
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-17 20:35:52 +0530

    add content for the directives

--- front-end/user-front/src/app/pages/shared/shared.module.ts
+++ front-end/user-front/src/app/pages/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { DirectivesDirective } from './directives/directives.directive';
+import { SvgParallaxDirective } from './directives/svg-parallax.directive';
 
 @NgModule({
-  declarations: [APP_DIRECTIVES],
+  declarations: [APP_DIRECTIVES, DirectivesDirective, SvgParallaxDirective],
   imports: [CommonModule],
   exports: [
     CommonModule,
