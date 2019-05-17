814181d71a4f35a8c2daf7b71bc5c161223178a2
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-17 18:36:33 +0530

    minor modifications

--- front-end/user-front/src/app/pages/pages.module.ts
+++ front-end/user-front/src/app/pages/pages.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { LandingModule } from './landing/landing.module';
+import { HeaderModule } from './header/header.module';
 
 const PAGES_COMPONENTS = [PagesComponent];
 
 @NgModule({
   declarations: [...PAGES_COMPONENTS],
-  imports: [CommonModule, PagesRoutingModule, LandingModule]
+  imports: [CommonModule, PagesRoutingModule, LandingModule, HeaderModule]
 })
 export class PagesModule {}
