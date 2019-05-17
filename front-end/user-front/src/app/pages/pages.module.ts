21140c84ceaf0805b706a43fd13d3c83f0737e06
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-17 12:26:34 +0530

    add pages/header component

--- front-end/user-front/src/app/pages/pages.module.ts
+++ front-end/user-front/src/app/pages/pages.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ]
