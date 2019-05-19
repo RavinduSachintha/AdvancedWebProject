66eeeef1b0be565afc23028aff848ed0121d33b7
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-19 17:06:46 +0530

    make changes in routing

--- front-end/user-front/src/app/pages/pages-routing.module.ts
+++ front-end/user-front/src/app/pages/pages-routing.module.ts
@@ -23,7 +23,6 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent
       },
-
       {
         path: 'signup',
         component: RegistrationComponent
--- front-end/user-front/src/app/pages/pages.module.ts
+++ front-end/user-front/src/app/pages/pages.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { HeaderModule } from './header/header.module';
+import { LandingModule } from './landing/landing.module';
+import { RegistrationModule } from './registration/registration.module';
+import { LoginModule } from './login/login.module';
 
 const PAGES_COMPONENTS = [PagesComponent];
 
 @NgModule({
   declarations: [...PAGES_COMPONENTS],
-  imports: [CommonModule, PagesRoutingModule, HeaderModule]
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    HeaderModule,
+    LandingModule,
+    RegistrationModule,
+    LoginModule
+  ]
 })
 export class PagesModule {}
