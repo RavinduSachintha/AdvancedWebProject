58112e700f6851bd45a95f3e1bc6ff4d338aae2a
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-19 23:40:39 +0530

    add index.ts

---
+++ front-end/user-front/src/app/pages/registration/validators/index.ts
@@ -0,0 +1,7 @@
+export { Country } from './country.model';
+export {
+  PasswordValidator,
+  ParentErrorStateMatcher
+} from './password.validator';
+export { PhoneValidator } from './phone.validator';
+export { UsernameValidator } from './username.validator';
