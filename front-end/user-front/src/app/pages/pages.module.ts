89e6676ea5b9b52cf9d9d1d6df02cb4748f8f868
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-19 14:45:54 +0530

    add registration component

--- front-end/user-front/src/app/pages/pages.module.ts
+++ front-end/user-front/src/app/pages/pages.module.ts
@@ -5,11 +5,12 @@ import { PagesComponent } from './pages.component';
 import { LandingModule } from './landing/landing.module';
 import { HeaderModule } from './header/header.module';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const PAGES_COMPONENTS = [PagesComponent];
 
 @NgModule({
-  declarations: [...PAGES_COMPONENTS, LoginComponent],
+  declarations: [...PAGES_COMPONENTS, LoginComponent, RegistrationComponent],
   imports: [CommonModule, PagesRoutingModule, LandingModule, HeaderModule]
 })
 export class PagesModule {}
