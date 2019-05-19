2f0617a01191a2b4360c539ed5ec94c8a2e8ce5a
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-19 23:39:45 +0530

    add username.validator.ts

---
+++ front-end/user-front/src/app/pages/registration/validators/username.validator.ts
@@ -0,0 +1,16 @@
+import { FormControl } from '@angular/forms';
+
+export class UsernameValidator {
+  static validUsername(fc: FormControl) {
+    if (
+      fc.value.toLowerCase() === 'abc123' ||
+      fc.value.toLowerCase() === '123abc'
+    ) {
+      return {
+        validUsername: true
+      };
+    } else {
+      return null;
+    }
+  }
+}
