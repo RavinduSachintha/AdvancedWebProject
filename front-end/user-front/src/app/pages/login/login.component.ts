a17f403771477cc3b6b6eaaec349d22767a73cf5
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-20 14:42:10 +0530

    make few changes in login component

--- front-end/user-front/src/app/pages/login/login.component.ts
+++ front-end/user-front/src/app/pages/login/login.component.ts
@@ -27,50 +27,23 @@ export class LoginComponent implements OnInit {
 
   parentErrorStateMatcher = new ParentErrorStateMatcher();
 
-  genders = ['Male', 'Female', 'Other'];
-
-  countries = [
-    new Country('UY', 'Uruguay'),
-    new Country('US', 'United States'),
-    new Country('AR', 'Argentina')
-  ];
-
-  validation_messages = {
-    fullname: [{ type: 'required', message: 'Full name is required' }],
-    bio: [
-      {
-        type: 'maxlength',
-        message: 'Bio cannot be more than 256 characters long'
-      }
-    ],
-    gender: [{ type: 'required', message: 'Please select your gender' }],
-    birthday: [{ type: 'required', message: 'Please insert your birthday' }],
-    phone: [
-      { type: 'required', message: 'Phone is required' },
-      {
-        type: 'validCountryPhone',
-        message: 'Phone incorrect for the country selected'
-      }
-    ]
-  };
-
   account_validation_messages = {
-    username: [
-      { type: 'required', message: 'Username is required' },
-      {
-        type: 'minlength',
-        message: 'Username must be at least 5 characters long'
-      },
-      {
-        type: 'maxlength',
-        message: 'Username cannot be more than 25 characters long'
-      },
-      {
-        type: 'pattern',
-        message: 'Your username must contain only numbers and letters'
-      },
-      { type: 'validUsername', message: 'Your username has already been taken' }
-    ],
+    // username: [
+    //   { type: 'required', message: 'Username is required' },
+    //   {
+    //     type: 'minlength',
+    //     message: 'Username must be at least 5 characters long'
+    //   },
+    //   {
+    //     type: 'maxlength',
+    //     message: 'Username cannot be more than 25 characters long'
+    //   },
+    //   {
+    //     type: 'pattern',
+    //     message: 'Your username must contain only numbers and letters'
+    //   },
+    //   { type: 'validUsername', message: 'Your username has already been taken' }
+    // ],
     email: [
       { type: 'required', message: 'Email is required' },
       { type: 'pattern', message: 'Enter a valid email' }
@@ -90,9 +63,6 @@ export class LoginComponent implements OnInit {
         message:
           'Your password must contain at least one uppercase, one lowercase, and one number'
       }
-    ],
-    terms: [
-      { type: 'pattern', message: 'You must accept terms and conditions' }
     ]
   };
 
@@ -122,33 +92,6 @@ export class LoginComponent implements OnInit {
         return PasswordValidator.areEqual(formGroup);
       }
     );
-
-    // country & phone validation
-    let country = new FormControl(this.countries[0], Validators.required);
-
-    let phone = new FormControl('', {
-      validators: Validators.compose([
-        Validators.required,
-        PhoneValidator.validCountryPhone(country)
-      ])
-    });
-
-    this.country_phone_group = new FormGroup({
-      country: country,
-      phone: phone
-    });
-
-    // user details form validations
-    this.userDetailsForm = this.fb.group({
-      fullname: ['Homero Simpson', Validators.required],
-      bio: [
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        Validators.maxLength(256)
-      ],
-      birthday: ['', Validators.required],
-      gender: new FormControl(this.genders[0], Validators.required),
-      country_phone: this.country_phone_group
-    });
 
     // user links form validations
     this.accountDetailsForm = this.fb.group({
