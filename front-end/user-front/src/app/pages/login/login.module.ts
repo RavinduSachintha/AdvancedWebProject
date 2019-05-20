4b10dfa39375489a57a5dcdfdd3f41b552dca834
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-20 14:01:09 +0530

    make changes in login

--- front-end/user-front/src/app/pages/login/login.module.ts
+++ front-end/user-front/src/app/pages/login/login.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSelectModule
+  ]
 })
 export class LoginModule {}
