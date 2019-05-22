d6184f09a995f821afd2d0e6629a31fafc1f729b
rashmi123456789 <32570239+rashmi123456789@users.noreply.github.com>
2019-05-23 00:18:06 +0530

    Search Function

--- front-end/User/src/app/app-routing.module.ts
+++ front-end/User/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ViewWordComponent } from "./view-word/view-word.component";
 import { CRUDwordsComponent } from "./crudwords/crudwords.component";
+import { SearchWordComponent } from "./search-word/search-word.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "insertword", pathMatch: "full" },
   { path: "viewword/:wordId", component: ViewWordComponent },
-  { path: "insertword", component: CRUDwordsComponent }
+  { path: "insertword", component: CRUDwordsComponent },
+  {path: "searchWord", component:SearchWordComponent}
 ];
 
 @NgModule({
