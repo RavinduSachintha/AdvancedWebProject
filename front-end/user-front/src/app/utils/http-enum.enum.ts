cd66ecfff68e51175b210218625adfe5429597d3
RavinduSachintha <ravindusachintha53@gmail.com>
2019-06-09 17:44:33 +0530

    delete unwanted app

--- front-end/user-front/src/app/utils/http-enum.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum HttpEnum {
-  USER_LOGIN = "http://localhost:3000/user/login",
-  USER_REGISTER = "http://localhost:3000/user/register",
-  USER_PROFILE = "http://localhost:3000/user/profile",
-  USER_EDIT_DETAILS = "http://localhost:3000/user/profile/editdetails/"
-}
