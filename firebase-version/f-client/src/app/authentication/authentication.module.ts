04f02259cb06bfc4a44a5c7057e18312f7a0e21a
RavinduSachintha <ravindusachintha53@gmail.com>
2019-08-07 00:31:58 +0530

    intitialized authetication module

---
+++ firebase-version/f-client/src/app/authentication/authentication.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { HelpPageComponent } from './help-page/help-page.component';
+
+
+
+@NgModule({
+  declarations: [LoginPageComponent, SignupPageComponent, HelpPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthenticationModule { }
