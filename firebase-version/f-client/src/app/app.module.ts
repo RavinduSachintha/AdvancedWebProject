1ac80bb28a4ec6805f82a7d61853f6ab1b8c9f99
RavinduSachintha <ravindusachintha53@gmail.com>
2019-08-07 00:41:14 +0530

    fixed modules

--- firebase-version/f-client/src/app/app.module.ts
+++ firebase-version/f-client/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AuthenticationModule } from "./authentication/authentication.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
+import { AppComponent } from "./app.component";
+import { environment } from "../environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AuthenticationModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule
@@ -20,4 +20,4 @@ import { environment } from '../environments/environment';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- firebase-version/f-client/src/app/authentication/authentication.module.ts
+++ firebase-version/f-client/src/app/authentication/authentication.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { HelpPageComponent } from './help-page/help-page.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { SignupPageComponent } from "./signup-page/signup-page.component";
+import { HelpPageComponent } from "./help-page/help-page.component";
 
 @NgModule({
   declarations: [LoginPageComponent, SignupPageComponent, HelpPageComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [LoginPageComponent, SignupPageComponent, HelpPageComponent]
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
